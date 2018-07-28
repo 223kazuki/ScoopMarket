4600d6129b023ee810608a6c93c91cf79b0b56e9
Kazuki Tsutsumi <tsutsumi@tisus.com>
2018-07-27 18:10:36 -0700

    Ropsten.

--- truffle.js
+++ truffle.js
@@ -1,5 +1,21 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.MNEMONIC;
+var accessToken = process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
   networks: {
+    ropsten: {
+      provider: function() {
+        var provider = new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/" + accessToken
+        );
+        console.log(provider.addresses)
+        return provider;
+      },
+      network_id: '*',
+      gas: 4700000
+    },
     development: {
       host: '127.0.0.1',
       port: 8545,
