2053d2e99457ad99261c97410022ae88e69c4d28
Kazuki Tsutsumi <tsutsumi@tisus.com>
2018-08-08 17:52:10 -0700

    Enabled purchase on contract.

--- contracts/Scoop.sol
+++ contracts/Scoop.sol
@@ -8,17 +8,17 @@ import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract Scoop is ERC721Token, Ownable {
     using SafeMath for uint;
 
-    struct ScoopStruct {
-        string name;
-        uint   timestamp;
-        string imageURI;
-        uint price;
-        bool forSale;
-        string metaDataURI;
+    struct ScoopToken {
+        string  name;
+        uint    timestamp;
+        string  imageURI;
+        uint    price;
+        bool    forSale;
+        string  metaDataURI;
         address author;
+        address requestor;
     }
-
-    ScoopStruct[] scoops;
+    ScoopToken[] scoops;
 
     uint public mintCost = 10 ** 16 wei;
 
@@ -27,7 +27,7 @@ contract Scoop is ERC721Token, Ownable {
     function mint(string _name, uint _price, bool _forSale, string _imageURI) external payable {
         require(msg.value == mintCost);
 
-        ScoopStruct memory _scoop = ScoopStruct(_name, block.timestamp, _imageURI, _price, _forSale, "", msg.sender);
+        ScoopToken memory _scoop = ScoopToken(_name, block.timestamp, _imageURI, _price, _forSale, "", msg.sender, address(0));
         uint tokenID = scoops.push(_scoop).sub(1);
 
         super._mint(msg.sender, tokenID);
@@ -41,14 +41,66 @@ contract Scoop is ERC721Token, Ownable {
         return ownedTokens[_address];
     }
 
-    function setTokenMetaDataUri(uint256 _tokenID, string _metaDataURI) external {
-        require(exists(_tokenID));
-        ScoopStruct storage scoop = scoops[_tokenID];
-        scoop.metaDataURI = _metaDataURI;
+    function scoopsForSale() external view returns (bool[]) {
+        bool[] memory result = new bool[](scoops.length);
+        for (uint i = 0; i < scoops.length; i ++) {
+            result[i] = scoops[i].forSale;
+        }
+        return result;
     }
 
-    function scoop(uint _tokenID) external view returns (uint, string, uint, string, uint, bool, string, address) {
-        ScoopStruct memory _scoop = scoops[_tokenID];
-        return (_tokenID, _scoop.name, _scoop.timestamp, _scoop.imageURI, _scoop.price, _scoop.forSale, _scoop.metaDataURI, _scoop.author);
+    function setTokenMetaDataUri(uint256 _tokenID, string _metaDataURI) external onlyOwnerOf(_tokenID) {
+        require(exists(_tokenID));
+        ScoopToken storage _scoop = scoops[_tokenID];
+        _scoop.metaDataURI = _metaDataURI;
+    }
+
+    function setTokenPrice(uint256 _tokenID, uint _price) external onlyOwnerOf(_tokenID) {
+        require(exists(_tokenID));
+        ScoopToken storage _scoop = scoops[_tokenID];
+        _scoop.price = _price;
+    }
+
+    function setTokenForSale(uint256 _tokenID, bool _forSale) external onlyOwnerOf(_tokenID) {
+        require(exists(_tokenID));
+        ScoopToken storage _scoop = scoops[_tokenID];
+        _scoop.forSale = _forSale;
+    }
+
+    function scoop(uint _tokenID) external view returns (uint, string, uint, string, uint, bool, string, address, address) {
+        ScoopToken memory _scoop = scoops[_tokenID];
+        return (_tokenID, _scoop.name, _scoop.timestamp, _scoop.imageURI, _scoop.price, _scoop.forSale, _scoop.metaDataURI, _scoop.author, _scoop.requestor);
+    }
+
+    function request(uint256 _tokenID) external payable {
+        require(scoops[_tokenID].price / 10 <= msg.value);
+        ScoopToken storage _scoop = scoops[_tokenID];
+        _scoop.requestor = msg.sender;
+    }
+
+    function approve(address _to, uint256 _tokenID) public {
+        require(_to == scoops[_tokenID].requestor);
+        super.approve(_to, _tokenID);
+    }
+
+    function purchase(uint256 _tokenID) external payable {
+        address oldOwner = tokenOwner[_tokenID];
+        address newOwner = msg.sender;
+        require(oldOwner != newOwner);
+        require(scoops[_tokenID].price <= msg.value);
+        require(scoops[_tokenID].forSale == true);
+        
+        ScoopToken memory _scoop = scoops[_tokenID];
+        uint price = _scoop.price;
+        require(msg.value >= price);
+
+        safeTransferFrom(oldOwner, newOwner, _tokenID);
+
+        scoops[_tokenID].forSale = false;
+        scoops[_tokenID].requestor = address(0);
+
+        if (price > 0) {
+            newOwner.transfer(price);
+        }
     }
 }
--- test/Scoop.js
+++ test/Scoop.js
@@ -2,17 +2,28 @@
 
 var Scoop = artifacts.require("Scoop");
 
-contract('Scoop', ([owner, user1, user2]) => {
+contract('Scoop', ([owner, user1, user2, user3]) => {
+
     it("should scoop works successfully.", async () => {
         let scoop = await Scoop.new();
-        await scoop.mint("QmSoPpGPFr3gz9rfwfwJuLahjTTmhdFJtKNvHYS58s8pqr", { from: user1, value: 10**16 });
-        await scoop.mint("QmSoPpGPFr3gz9rfwfwJuLahjTTmhdFJtKNvHYS58s8pqr", { from: user2, value: 10**16 });
-        await scoop.mint("QmSoPpGPFr3gz9rfwfwJuLahjTTmhdFJtKNvHYS58s8pqr", { from: user1, value: 10**16 });
+        await scoop.mint("Scoop1", 10**15, true,  "QmSoPpGPFr3gz9rfwfwJuLahjTTmhdFJtKNvHYS58s8pqr", { from: user1, value: 10**16 });
+        await scoop.mint("Scoop2", 10**15, true,  "QmSoPpGPFr3gz9rfwfwJuLahjTTmhdFJtKNvHYS58s8pqr", { from: user1, value: 10**16 });
+        await scoop.mint("Scoop3", 10**15, false, "QmSoPpGPFr3gz9rfwfwJuLahjTTmhdFJtKNvHYS58s8pqr", { from: user1, value: 10**16 });
+
         let balance = await scoop.balanceOf(user1);
         console.log(balance.toNumber());
+
         let tokens = await scoop.scoopsOf(user1);
         console.log(tokens);
-        let tokenURI = await scoop.tokenURI(tokens[0].toNumber());
-        console.log(tokenURI);
+
+        let tokensForSale = await scoop.scoopsForSale();
+        console.log(tokensForSale);
+
+        await scoop.request(0, { from: user2, value: 10**14 });
+        await scoop.approve(user2, 0, { from: user1 });
+        await scoop.purchase(0, { from: user2, value: 10**15 });
+
+        let balance2 = await scoop.balanceOf(user2);
+        console.log(balance2.toNumber());
     });
 });
