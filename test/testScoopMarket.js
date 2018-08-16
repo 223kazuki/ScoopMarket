5277085d504465b8f377775b09f516bfa31667e8
Kazuki Tsutsumi <kazuki@Kazuki-MBP.local>
2018-08-15 23:37:53 -0700

    Wrote test.

--- test/testScoopMarket.js
+++ test/testScoopMarket.js
@@ -15,6 +15,7 @@ const checkReverted = async (promise) => {
 contract('ScoopMarket', ([owner, user1, user2, user3]) => {
     let scoopMarket;
     let testHash = "QmSoPpGPFr3gz9rfwfwJuLahjTTmhdFJtKNvHYS58s8pqr";
+    let zeroAddress = "0x0000000000000000000000000000000000000000";
 
     beforeEach('setup contract for each test', async function () {
         scoopMarket = await ScoopMarket.new({ from: owner });
@@ -22,9 +23,9 @@ contract('ScoopMarket', ([owner, user1, user2, user3]) => {
 
     // As a Proof of Existence dApp
     it("should mint scoops successfully.", async () => {
+        await scoopMarket.mint("scoop0", 10**15, true,  testHash, { from: user1 });
         await scoopMarket.mint("scoop1", 10**15, true,  testHash, { from: user1 });
-        await scoopMarket.mint("scoop2", 10**15, true,  testHash, { from: user1 });
-        await scoopMarket.mint("scoop3", 10**15, false, testHash, { from: user1 });
+        await scoopMarket.mint("scoop2", 10**15, false, testHash, { from: user1 });
 
         let balance = await scoopMarket.balanceOf(user1);
         assert(balance.toNumber() === 3, "Balance is correct.");
@@ -39,12 +40,45 @@ contract('ScoopMarket', ([owner, user1, user2, user3]) => {
         assert(tokensForSale[1], "Token 1 ID is for sale.");
         assert(!tokensForSale[2], "Token 2 ID is not for sale.");
 
-        // await scoopMarket.request(0, { from: user2, value: 10**14 });
-        // await scoopMarket.approve(user2, 0, { from: user1 });
-        // await scoopMarket.purchase(0, { from: user2, value: 10**15 });
-
-        // let balance2 = await scoopMarket.balanceOf(user2);
-        // console.log(balance2.toNumber());
+        {
+            let scoop0 = await scoopMarket.scoop(0);
+            let [id, name, _, imageURI, price, forSale, metaDataURI, author, owner, requestor] = scoop0;
+            assert(id.toNumber() === 0, "scoop0 id is correct.");
+            assert(name === "scoop0", "scoop0 name is correct.");
+            assert(imageURI === testHash, "scoop0 imageURI is correct.");
+            assert(price.toNumber() === 10**15, "scoop0 price is correct.");
+            assert(forSale, "scoop0 forSale is correct.");
+            assert(metaDataURI === "", "scoop0 metaDataURI is correct.");
+            assert(author === user1, "scoop0 author is correct.");
+            assert(owner === user1, "scoop0 owner is correct.");
+            assert(requestor === zeroAddress, "scoop0 requestor is correct.");
+        }
+        {
+            let scoop1 = await scoopMarket.scoop(1);
+            let [id, name, _, imageURI, price, forSale, metaDataURI, author, owner, requestor] = scoop1;
+            assert(id.toNumber() === 1, "scoop1 id is correct.");
+            assert(name === "scoop1", "scoop1 name is correct.");
+            assert(imageURI === testHash, "scoop1 imageURI is correct.");
+            assert(price.toNumber() === 10**15, "scoop1 price is correct.");
+            assert(forSale, "scoop1 forSale is correct.");
+            assert(metaDataURI === "", "scoop1 metaDataURI is correct.");
+            assert(author === user1, "scoop1 author is correct.");
+            assert(owner === user1, "scoop1 owner is correct.");
+            assert(requestor === zeroAddress, "scoop1 requestor is correct.");
+        }
+        {
+            let scoop2 = await scoopMarket.scoop(2);
+            let [id, name, _, imageURI, price, forSale, metaDataURI, author, owner, requestor] = scoop2;
+            assert(id.toNumber() === 2, "scoop2 id is correct.");
+            assert(name === "scoop2", "scoop2 name is correct.");
+            assert(imageURI === testHash, "scoop2 imageURI is correct.");
+            assert(price.toNumber() === 10**15, "scoop2 price is correct.");
+            assert(!forSale, "scoop2 forSale is correct.");
+            assert(metaDataURI === "", "scoop2 metaDataURI is correct.");
+            assert(author === user1, "scoop2 author is correct.");
+            assert(owner === user1, "scoop2 owner is correct.");
+            assert(requestor === zeroAddress, "scoop2 requestor is correct.");
+        }
     });
     it("should not mint scoop with insufficient value.", async () => {
         checkReverted(
@@ -57,28 +91,36 @@ contract('ScoopMarket', ([owner, user1, user2, user3]) => {
         checkReverted(
             scoopMarket.mint("scoop2", 10**15, true,  testHash, { from: user1 })
         );
-        // let balance = await owner.getBalance();
         let balanceBefore = web3.eth.getBalance(owner);
-        let result = await scoopMarket.withdrawPayments({ from: owner });
+        await scoopMarket.withdrawPayments({ from: owner });
         let balanceAfter = web3.eth.getBalance(owner);
-        let gasPrice = web3.eth.getTransaction(result.tx).gasPrice.toNumber();
-        let cost = gasPrice * result.receipt.gasUsed;
-
-        assert(balanceAfter.toNumber() - balanceBefore.toNumber() + cost === 10**16, "Balance is correct.");
+        assert(balanceAfter.toNumber() > balanceBefore.toNumber(), "Withdrawal correctly.");
     });
     it("should not mint scoop with invalid input parameters.", async () => {});
-    it("should edit scoop successfully.", async () => {});
+    it("should edit scoop successfully.", async () => {
+        await scoopMarket.mint("scoop1", 10**15, true,  testHash, { from: user1 });
+    });
     it("should not edit scoop of others.", async () => {});
 
     // As a Market dApp
-    it("should enable user to purchase only approved token.", async () => {});
-    it("should.", async () => {});
+    it("buyer should be able to purchase only approved token.", async () => {
+        // await scoopMarket.request(0, { from: user2, value: 10**14 });
+        // await scoopMarket.approve(user2, 0, { from: user1 });
+        // await scoopMarket.purchase(0, { from: user2, value: 10**15 });
+
+        // let balance2 = await scoopMarket.balanceOf(user2);
+        // console.log(balance2.toNumber());
+    });
+    it("buyer should be able to request only token for sale.", async () => {});
+    it("buyer should be able to cancel request.", async () => {});
+    it("buyer should be able to cancel approval.", async () => {});
+    it("seller should be able to approve request.", async () => {});
+    it("seller should be able to deny request.", async () => {});
 
     // System requirements
     it("should do emergency stop.", async () => {
         await scoopMarket.pause({ from: owner });
         checkReverted(
-            scoopMarket.mint("scoop1", 10**15, true,  "QmSoPpGPFr3gz9rfwfwJuLahjTTmhdFJtKNvHYS58s8pqr", { from: user1, value: 10**16 })
         );
     });
 
