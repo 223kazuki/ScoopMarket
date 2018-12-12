bcd005aec18d366a81df6a52957dce6d630d7cf0
Kazuki Tsutsumi <tsutsumi@tisus.com>
2018-12-12 09:51:24 -0800

    Remove unncessary files.

--- deploy_heroku.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-lein do clean, build
-heroku git:remote -a scoopmarket
-git checkout -B deploy
-git add -f resources/public
-git commit -m "deploy"
-git push --force heroku deploy:master
-git checkout master
